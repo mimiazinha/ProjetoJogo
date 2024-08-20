e4bd2f06db8616b8dcd9a6d2d027d9dcc2a88359
mimia <milenaakkjjk@gmail.com>
2024-08-20 18:49:04 -0300

    documentando codigo

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 //xablaudnv
 //const prompt = require('prompt-sync')();
 
-
+// inclui os sentimentos no inventario de acordo com as escolhas realizadas durante o jogo
 function incluirSentimento(sentimento){
     if(!inventario.includes(sentimento)){
         inventario.push(sentimento)
     }
 }
+//remove os sentimentos do inventario caso a pessoa tenha escolhido utilizar o sentimento para nao ser penalizada
 function removeSentimento(sentimento){
     let index = inventario.indexOf(sentimento);
     //alert("inventario "+ inventario + " posição " + index)
@@ -15,7 +16,7 @@ function removeSentimento(sentimento){
         inventario.splice(index, 1);
     }
 }
-
+// coloca um valor inicial para todas as variaveis
 function inicializarVariaveis() { 
     contVida = 4;
     inventario = [];
@@ -27,18 +28,18 @@ function inicializarVariaveis() {
     mantemProgresso1 = false;
     //alert("Inventario limpo.");
 }
-
+//primeira fase do jogo
 function fase1(){
     if ((contVida <= 0) && !mantemProgresso1 && !checkpoint2 && !checkpoint3){
         inicializarVariaveis();
     }
-    
-    alert("Esse jogo é baseado na história de Cindy Ngamba, seu objetivo é fazer as escolhar certas e juntar pontos para que você possa chegar nas olimpíedas, você também terá um inventario de sentimentos(que poderá utilizar como ajuda em caso de escolhas erradas) a partir da terceira fase. Boa sorte.")
+    //informativos sobre objetivo do jogo e boas vindas.
+    alert("Esse jogo é baseado na história de Cindy Ngamba, seu objetivo é fazer as escolhas certas e juntar pontos para que você possa chegar nas olimpíedas, você também terá um inventario de sentimentos(que poderá utilizar como ajuda em caso de escolhas erradas) a partir da terceira fase. Boa sorte.")
     
     alert("Bem-vindo(a), espero que goste do jogo. \n >>>REFUSENT<<< \n O jogo onde suas escolhas te levam ao sucesso…")
-
+//faz uma pergunta e o jogador devera responder conforme as opções
     let resposta = prompt("Essa será sua primeira escolha: Digite \n 1- Treinar\n 2- Descansar")
-
+//se resposta for positiva, o jogador recebera pontos e beneficios(os sentimentos que serao incluidos no inventario)
     if(resposta == 1){
         resposta = prompt("Seus amigos estão na academia e te convidaram para treinar junto, o que você vai fazer? Digite \n 1- Treinar com seus amigos\n 2- Recusar o convite e treinar em casa.")
         if(resposta == 1){
@@ -52,7 +53,7 @@ function fase1(){
             incluirSentimento("solidão")
         }
         
-        
+        //em caso de resposta negativa, o jogador sera testado duas vezes e dependendo de sua resposta, o jogador recebera tal sentimento
     }else if(resposta == 2){
         let resposta1
         
@@ -76,7 +77,7 @@ function fase1(){
             //cansaço físico ou mental.
         }
         else {
-        
+        //se jogador colocar "não" nas duas perguntas, ele perdera pontos e recebera um sentimento negativo.
         }
         if(resposta1 && resposta2 == 2){
             contVida--
@@ -84,11 +85,10 @@ function fase1(){
         }
         
     }
+    //mostra inventario atual
 alert("inventario: "+ inventario)
-    //if(inventario.includes("preguiça")){
-        //fazer codigo para tirar preguiça
-    //}
-
+    
+// alerta de que conclui fase 1 e checkpoint(caso o jogador queira salvar o jogo, quando der gameover, o jogador podera voltar para a fase em que fez o checkpoint)
     alert("parabens! você concluiu a fase 1.")
     resposta = prompt("Deseja salvar seu progresso? Digite \n 1- Sim\n 2- Não")
     if(resposta == 1){
@@ -100,11 +100,11 @@ alert("inventario: "+ inventario)
     }
 
     
-    //alert("contVida cena 1: "+ contVida)
     
-    return (contVida <= 0); //Retornando se é gameover ou não
+    
+    return (contVida <= 0); //Retorna se é gameover ou não
 }
-
+// da os parabens por chegar na fase dois e começa outro desafio que o jogador tera de escolher a melhor opção
 function fase2() {
      alert("Parabéns! Você chegou na fase 2. ")
     alert("É seu aniversário! Sua mãe te perguntou qual presente você vai querer.")
@@ -113,9 +113,10 @@ function fase2() {
     if(resposta == 1){
         contVida++;
         resposta1 = prompt("Sua mãe ofereceu para pagar um treinamento de box com um treinador olímpico, você vai aceitar? Digite \n 1- Sim \n 2- Não.")
-       
+       //se responder que sim, aparecera outra pergunta
         if(resposta1 == 1){
-            
+            //testa se a resposta é um numero e se é entre o numero 1 e o numero 7(pois a semana tem 7 dias)
+            //enquanto nao for digitado um numero conforme os requisitos acima, vai continuar em loop
             while(true){
              let qntDias = Number(prompt("Quantos dias de treinamento você vai fazer por semana?"))
              if(qntDias != 'NaN' && qntDias > 0 && qntDias < 8) {
@@ -125,15 +126,17 @@ function fase2() {
                     contVida = contVida + 5
                 } 
                 break; 
+                //avisa que digitou fora dos parametros esperados
             } else{
                 alert("Hein???? Acho que você digitou errado... ")
             }
         }
     }
+    //caso responda nao, o jogador perdera pontos
     }else{
         contVida--;
     }
-
+//outro checkpoint
    
         resposta = prompt("Deseja salvar seu progresso? Digite \n 1- Sim\n 2- Não")
         if(resposta == 1){
@@ -143,35 +146,37 @@ function fase2() {
             checkpoint2 = false;
         }
 
-    //alert("contVida cena 2: "+ contVida)
+    //inventario
     alert("inventario: "+ inventario)
     
     return (contVida <= 0); //Retornando se é gameover ou não
 }
-
+//fase tres e ultima fase do jogo(nessa etapa é possivel utilizar o inventario para obter beneficios)
 function fase3() {
     alert("parabens! você chegou na fase 3. ")
     
     resposta = prompt("Suas notas na escola não estão boas e sua mãe disse que se não melhorar, terá que desistir do boxe. Oque você vai fazer? Digite \n 1- Estudar mais para melhorar as notas \n 2- Desistir de ir para as olimpíadas")
-
+//pergunta algo e conforme a resposta vai adicionar ou remover pontos
     if(resposta == 1){
         contVida = contVida + 2;
+        
     }else{
+        //permite que o jogador use os sentimentos que tem no inventario para obter beneficios(nao ser penalizado/nao perder pontos)
         resposta = prompt("Você tem certeza? Você possui 'MOTIVAÇÃO' no seu inventario, deseja utilizar? Digite: \n 1- Sim \n 2- Não.")
         if(resposta == 1){
-
+//tira o sentimento do inventario pois o jogador usou
             removeSentimento("motivação")
         } else {
         contVida = contVida - 7; }
     }
-
+//outras perguntas
     resposta = prompt("Você foi convidada para jogar jogos pré olímpicos, você vai aceitar? Digite \n 1- Sim \n 2- Não")
-
+//remove ou adiciona pontos conforme a resposta do jogador
     if(resposta == 1){
         contVida = contVida + 2;
-    }
+    }//checkpoint
         resposta = prompt("Deseja salvar seu progresso? Digite \n 1- Sim\n 2- Não")
-       
+       //coloca falso nos outros checkpoints para que se o jogador escolher salvar, o jogo continue na fase em que o jogador salvou e caso ele nao salve, torna falso o 3
         if(resposta == 1){
             checkpoint1 = false;
             checkpoint2 = false;
@@ -180,35 +185,34 @@ function fase3() {
             checkpoint3 = false;
         }
 
-    //alert("contVida cena 3: "+ contVida)
-    //alert("inventario: "+ inventario)
+   
     
     return (contVida <= 0); //Retornando se é gameover ou não
 }
 
-
+// cena da vitoria e um link para mais informacoes sobre a atleta(o link abre uma nova aba)
 function cenaVitoria() {
     alert("Vitoria!!!! \n Você chegou nas olimpíadas! \n obrigada por jogar <3 \n Essa é você atualmente: \n ")
     window.open('https://en.wikipedia.org/wiki/Cindy_Ngamba', '_blank');
 
 }
 
-var contVida = 4;
-var inventario = [];
-var recomecarJogo = true;
-var ehgameOver = false;
-var checkpoint1 = false;
-var checkpoint2 = false;
-var checkpoint3 = false;
-var mantemProgresso1 = false;
+var contVida = 4; //pontos
+var inventario = []; //inventario
+var recomecarJogo = true; //recomeca o jogo
+var ehgameOver = false; //da gameover
+var checkpoint1 = false; //checkpoint da primeira fase
+var checkpoint2 = false; //checkpoint segunda fase
+var checkpoint3 = false; //checkpoint da terceira fase
+var mantemProgresso1 = false; //mantem os pontos e o inventario caso de gameover e o jogador tenha salvado o jogo
 
-while(true){
-    if (checkpoint1 || recomecarJogo){
-        ehgameOver = fase1();
-        if (contVida >= 15) {
+while(true){ // enquanto for verdadeiro
+    if (checkpoint1 || recomecarJogo){ //se checkpoint 1 ou recomecar jogo(reinicia o jogo limpando o inventario e os pontos do jogador)
+        ehgameOver = fase1(); //fase 1 recebe gameover(a variavel gameover foi criada como falso entao na fase1, dentro da função, caso o jogo chegue a 0, o gameover sera true)
+        if (contVida >= 15) { // 15 é o maximo de pontos que o jogador pode chegar, entao quando o jogador chegar a 15 pontos, a função cenaVitoria sera chamada e ira encerrar o jogo.
             cenaVitoria()
             break;
-        } else if(ehgameOver) {
+        } else if(ehgameOver) { //avisara quando der gameover
             alert("Game Over");
         }
     }
@@ -231,5 +235,5 @@ while(true){
         }
     }
 
-    recomecarJogo = (!checkpoint1 && !checkpoint2 && !checkpoint3);
+    recomecarJogo = (!checkpoint1 && !checkpoint2 && !checkpoint3); //recomeça o jogo zerando o inventario e os pontos do jogador. (e volta para o inicio do jogo)
 }
